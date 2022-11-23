e4f063f4d63d6e3f406bd4d214196a1e12dcd2ad
Junghawn You <dnemfdlemf@naver.com>
2022-11-23 14:34:08 +0900

    main_frame3 upload

--- src/component/Main/Frame_3.js
+++ src/component/Main/Frame_3.js
@@ -5,7 +5,55 @@ function Frame_3(props) {
         <div>
             <section>
                 <div className='frame_3'>
-        
+                    <div className='frame_3_contentbox'>
+                        <div className='frame3_content_1'>
+                                <h1>송금</h1>
+                                <h2>간편하고 안전하게<br/>
+                                수수료는 평생 무료로,<br/>
+                                이런 송금 써보셨나요?</h2>
+                        </div>
+                        <div className='frame3_content_2'>
+                            <div>
+                                <h1><i class="won sign icon"></i>평생 무료 송금</h1>
+                                <h2>토스 평생 무료송금으로<br/>
+                                모두의 금융에 자유를</h2>
+                                <h3>누구에게 보내든 은행 상관 없이,<br/>
+                                이제 토스와 함께 수수료 걱정 없이 송금하세요.</h3>
+                            </div>
+                            <div>
+                                <img src="https://static.toss.im/assets/homepage/newtossim/section1_2_01.png"></img>
+                            </div>
+                        </div>
+
+                        <div className='frame3_content_3'>
+                            <div>
+                                <img src="https://static.toss.im/assets/homepage/newtossim/section1_2_02.png"></img>
+                            </div>
+                        
+                            <div>
+                                <h1><i class="shield alternate icon"></i>사기계좌 조회</h1>
+                                <h2>송금 전 사기 내역 조회로<br/>
+                                피해를 미리 방지할 수 있어요</h2>
+                                <h3>송금 전 토스가 알아서 사기 내역 조회를 해드려요.<br/>
+                                상대방의 연락처 또는 계좌가 사기 계좌인지 조회해<br/>
+                                안전하게 송금할 수 있어요.
+                                </h3>
+                            </div>
+                        </div>
+
+                        <div className='frame3_content_4'>
+                            <div>
+                                <h1><i class="time icon"></i>자동이체 예약</h1>
+                                <h2>은행 점검 시간,<br/>
+                                기다릴 필요 없어요</h2>
+                                <h3>은행 점검 시간에는 자동이체 예약을 이용해보세요.<br/>
+                                점검 시간이 끝나면 알아서 송금해드릴게요.</h3>
+                            </div>
+                            <div>
+                                <img src="https://static.toss.im/assets/homepage/newtossim/section1_2_03.png"></img>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
