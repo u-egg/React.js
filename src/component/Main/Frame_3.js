a5bd475777b8e0296323557ebbc2077e5f6061c0
Junghawn You <dnemfdlemf@naver.com>
2022-11-23 13:54:38 +0900

    CloneCoding_toss

---
+++ src/component/Main/Frame_3.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Frame_3(props) {
+    return (
+        <div>
+            <section>
+                <div className='frame_3'>
+        
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Frame_3;
