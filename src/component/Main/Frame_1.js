82d712ba584d051711c988f8b8c26c3328fc09f7
Junghawn You <dnemfdlemf@naver.com>
2022-12-09 10:35:09 +0900

    frame_1 애니메이션 및 이동 이벤트 추가

--- src/component/Main/Frame_1.js
+++ src/component/Main/Frame_1.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import $ from 'jquery';
+
+function scrollMove(){ //특정 위치로 스크롤 이동시키는 함수
+    var offset = $("#next_section").offset(); // 해당 id 위치값 반환
+    $("html").animate({scrollTop:offset.top},1000); // 그 곳으로 애니메이션 효과 넣어서 이동
+}
 
 function Frame_1(props) {
     return (
@@ -6,6 +12,10 @@ function Frame_1(props) {
             <section className='main_box_top'>
                 <div className='bg_frame'>
                     <img className='bg_img' src='https://static.toss.im/assets/homepage/newtossim/new_main.png'></img>
+                    
+                    <div className='move_icon' onClick={scrollMove}>
+                        <i class="chevron down icon"></i>
+                    </div>
                 </div>
                 <div className='frame_1_text'>
                     <h1>금융의 모든 것<br/>
@@ -13,7 +23,7 @@ function Frame_1(props) {
                     <button><i class="apple icon"></i> App Store</button>
                     <button><i class="google play icon"></i> Google Play</button>
                 </div>
-                <div className='frame_1_text2'>
+                <div className='frame_1_text2' id='next_section'>
                     <p>
                     내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.<br/>
                     이제껏 경험 못 했던 쉽고 편리한 금융 서비스,<br/>
